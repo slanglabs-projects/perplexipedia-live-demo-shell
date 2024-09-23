ebdf2f8c0d249b110fb0ebd4bd6642a9a71abebd
Giridhar Murthy <gmurthy@slanglabs.in>
2024-09-22 23:27:38 -0700

    [Omni][Demo] Simulate progress since real progress is no longer available

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from conva_ai import ConvaAI
-from utils import (SourceItem, get_md_normal_text, get_md_hyperlink, extract_citations)
+from utils import SourceItem, get_md_normal_text, get_md_hyperlink, extract_citations
 
-import os
 import streamlit as st
+import threading
+import time
+
+task_complete = threading.Event()
 
 ASSISTANT_ID = "547979d3e0b541baba824a2919623401"
 API_KEY = "68e509b36d54471e8433d06f09a3207b"
 ASSISTANT_VERSION = "1.0.0"
 
+
+PROGRESS_MESSAGES = {
+    0: "Searching for relevant information...",
+    30: "Processing search results...",
+    60: "Reading results...",
+    90: "Generating answer...",
+}
+
 st.set_page_config(page_title="Perplexipedia - by Conva.AI")
 
-
-# Hack to get playwright to work properly
-os.system("playwright install")
 
 if "status" not in st.session_state:
     st.session_state.status = "uninitialized"
@@ -30,20 +38,53 @@ if "answer" not in st.session_state:
     st.session_state.answer = ""
 
 
+def get_answer_and_postprocess(query, results):
+    response = get_answer(query)
+    response, sources, answer = handle_response(response)
+    results["response"] = response
+    results["sources"] = sources
+    results["answer"] = answer
+    task_complete.set()
+
+
+def simulate_progress_update():
+    progress_bar = pbph.progress(0, "Searching for relevant information...")
+    progress = 0
+    while not task_complete.is_set():
+        # Simulate progress updates
+        time.sleep(4)
+        progress += 30
+        progress = min(progress, 90)
+        progress_bar.progress(progress, PROGRESS_MESSAGES[progress])
+    progress_bar.progress(100)
+
+
 def execute_action(key):
     st.session_state.status = "processing"
     st.session_state.query_value = st.session_state[key]
     if ASSISTANT_ID and API_KEY:
-        response = get_answer(st.session_state[key])
-        handle_response(response)
+        results = {}
+        threading.Thread(target=get_answer_and_postprocess, args=(st.session_state[key], results)).start()
+        simulate_progress_update()
+        task_complete.wait()
+        st.session_state.status = "success"
+        st.session_state.response = results.get("response")
+        st.session_state.sources = results.get("sources")
+        st.session_state.answer = results.get("answer")
 
 
 def execute_action_btn(value):
     st.session_state.status = "processing"
     st.session_state.query_value = value
     if ASSISTANT_ID and API_KEY:
-        response = get_answer(value)
-        handle_response(response)
+        results = {}
+        threading.Thread(target=get_answer_and_postprocess, args=(value, results)).start()
+        simulate_progress_update()
+        task_complete.wait()
+        st.session_state.status = "success"
+        st.session_state.response = results.get("response")
+        st.session_state.sources = results.get("sources")
+        st.session_state.answer = results.get("answer")
 
 
 def reset():
@@ -73,22 +114,11 @@ if st.session_state.status == "uninitialized":
 
 
 def get_answer(query):
-    progress = 5
-    pb = pbph.progress(progress, "Searching for relevant information...")
-
-    progress += 50
-    pb.progress(progress, "Processing search results...")
-
-    pb.progress(progress, "Reading results...")
-
     client = ConvaAI(
         assistant_id=ASSISTANT_ID,
         api_key=API_KEY,
         assistant_version=ASSISTANT_VERSION,
     )
-
-    progress += 25
-    pb.progress(progress, "Generating answer...")
 
     response = client.invoke_capability_name(
         query="Answer the user's query based on the provided context. User's query: ({})".format(query),
@@ -96,14 +126,10 @@ def get_answer(query):
         timeout=600,
         stream=False,
     )
-
-    pb.progress(100, "Completed")
-    st.session_state.status = "success"
     return response
 
 
-def postprocess_response(response):
-    sources = st.session_state.sources
+def postprocess_response(response, sources):
     citations = extract_citations(response)
 
     final_sources = {k: v for k, v in sources.items() if k in citations}
@@ -118,11 +144,9 @@ def postprocess_response(response):
 
 def handle_response(response):
     tmp = response.parameters.get("sources", [])
-    st.session_state.sources = {"cit{}".format(i+1): SourceItem("cit{}".format(i+1), tmp[i], "") for i in range(len(tmp))}
-    sources, answer = postprocess_response(response.message)
-    st.session_state.response = response
-    st.session_state.sources = sources
-    st.session_state.answer = answer
+    sources = {"cit{}".format(i + 1): SourceItem("cit{}".format(i + 1), tmp[i], "") for i in range(len(tmp))}
+    sources, answer = postprocess_response(response.message, sources)
+    return response, sources, answer
 
 
 if st.session_state.status == "success":
--- utils.py
+++ utils.py
@@ -1,40 +1,5 @@
 import re
 
-import requests
-import streamlit as st
-import tiktoken
-
-BING_SEARCH_API_KEY = st.secrets.bing_api_key
-
-
-def do_custom_search(query: str) -> dict:
-    headers = {"Ocp-Apim-Subscription-Key": BING_SEARCH_API_KEY}
-    params = {
-        "q": query,
-        "customConfig": "7469e414-57a0-4289-898c-bf9f1fbfd380",
-        "count": 5,
-    }
-    response = requests.get(
-        "https://api.bing.microsoft.com/v7.0/custom/search",
-        headers=headers,
-        params=params,
-    )
-    response.raise_for_status()
-    search_results = response.json()
-
-    urls = []
-    source_items = {}
-    for index, sr in enumerate(search_results["webPages"]["value"]):
-        url = sr["url"]
-        snippet = sr["snippet"]
-        if url not in urls:
-            urls.append(url)
-            id = "cit{}".format(index)
-            source_items[id] = SourceItem("cit{}".format(index), url, snippet)
-
-    return source_items
-
-
 class SourceItem:
     def __init__(self, id, url, snippet, content=None, index=-1):
         self.id = id
@@ -44,29 +9,10 @@ class SourceItem:
         self.index = index
 
 
-def num_tokens_from_string(string: str, model_name: str) -> int:
-    encoding = tiktoken.encoding_for_model(model_name)
-    num_tokens = len(encoding.encode(string))
-    print("num tokens = {}".format(num_tokens))
-    return num_tokens
-
-
 def escape_braces(text: str) -> str:
     text = re.sub(r"(?<!\{)\{(?!\{)", r"{{", text)  # noqa
     text = re.sub(r"(?<!\})\}(?!\})", r"}}", text)  # noqa
     return text
-
-
-def maybe_trim_context(context: str) -> str:
-    length = len(context)
-    tokens = num_tokens_from_string(context, "gpt-4o-mini")
-    start = 0
-    finish = length
-    while tokens > 120 * 1000:
-        finish = int(finish - 0.1 * finish)
-        context = context[start:finish]
-        tokens = num_tokens_from_string(context, "gpt-4o-mini")
-    return context
 
 
 def get_md_normal_text(text):
